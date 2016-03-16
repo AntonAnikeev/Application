91f3d83d7d2dad431ab035aee4a4db9e3b0f58ce
anton <anton.anikeev.home@gmail.com>
2016-03-16 15:52:27 +0200

    Fix taskService implementation

--- WebApp/src/mainContext/taskService.js
+++ WebApp/src/mainContext/taskService.js
@@ -2,7 +2,9 @@
  * Created by admin on 3/16/2016.
  */
 angular.module('mainContext')
-    .service('taskService',function(name,intValue){
-        return {name: name, intValue: intValue};
+    .service('taskService',function(){
+        this.getTask = function(name,intValue){
+            return{name: name, intValue: intValue};
+        };
     });
 
