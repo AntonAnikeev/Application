7f3e94c78162f634b2c7ba58f240e9227bde5ad4
anton <anton.anikeev.home@gmail.com>
2016-03-28 17:59:17 +0300

    Add attributes to dtos

--- Server/ApiContracts/TaskDto.cs
+++ Server/ApiContracts/TaskDto.cs
@@ -1,9 +1,14 @@
+using System.Runtime.Serialization;
+
 namespace ApiContracts
 {
+    [DataContract]
     public class TaskDto
     {
+        [DataMember(Name = "id")]
         public string Id { get; set; }
 
+        [DataMember(Name = "content")]
         public string Content { get; set; }
     }
 }
--- Server/ApiContracts/TaskListDto.cs
+++ Server/ApiContracts/TaskListDto.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace ApiContracts
 {
+    [DataContract]
     public class TaskListDto
     {
+        [DataMember(Name = "id")]
         public string Id { get; set; }
 
+        [DataMember(Name = "name")]
         public string  Name { get; set; }
 
+        [DataMember(Name = "taskNumber")]
         public int TaskNumber { get; set; }
 
+        [DataMember(Name = "tasks")]
         public List<TaskDto> Tasks { get; set; }
     }
 }
