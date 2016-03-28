46f8edbc3ebc5df9ba94c559a32eee729c760a1a
anton <anton.anikeev.home@gmail.com>
2016-03-28 16:42:06 +0300

    taskListService: add model class

--- WebApp/src/common/restManager.js
+++ WebApp/src/common/restManager.js
@@ -4,11 +4,24 @@
 
 angular.module('common')
     .factory('restManager', ['Restangular', function (Restangular) {
+
+        Restangular.setErrorInterceptor(function(response, deferred, responseHandler) {
+            //if(response.status === 403) {
+            //    });
+            //    return false; // error handled
+            //}
+            //return true; // error not handled
+        });
+
         var getDataList = function (url) {
             if(url){
-               return Restangular.all('url').getList();
+               return Restangular.all(url).getList();
             }
             throw "restManaget.getDataList: url parameter is undefined!"
+        };
+
+        var getDataEntity = function(params){
+
         };
 
         var deleteData = function () {
@@ -17,7 +30,8 @@ angular.module('common')
         var updateData = function () {
         };
         return {
-            getData: getData(),
+            getDataList: getDataList(url),
+            getDataEntity:getDataEntity(params),
             deleteData: deleteData(),
             updateData: updateData()
         };
--- WebApp/src/mainContext/taskListContainer/taskListService.js
+++ WebApp/src/mainContext/taskListContainer/taskListService.js
@@ -9,5 +9,21 @@
 
 angular.module('mainContext')
     .service('taskListService',['',function(){
+        var TaskListModel = function(data)
+        {
+            if (data) {
+                this.setData(data);
+            }
+        };
+
+        TaskListModel.prototype.setData =function(data){
+            return angular.extend(this, data);
+        };
+
+        TaskListModel.prototype.getAll = function(){
+                
+        };
+
+
 
     }]);
