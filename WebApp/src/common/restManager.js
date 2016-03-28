a9a346f870721644c51544a53e3b4f35f8380153
anton <anton.anikeev.home@gmail.com>
2016-03-28 15:38:53 +0300

    restManager: implement getDataList method

--- WebApp/src/common/restManager.js
+++ WebApp/src/common/restManager.js
@@ -3,5 +3,22 @@
  */
 
 angular.module('common')
-    .service('restManager',['Restangular',function(Restangular){
+    .factory('restManager', ['Restangular', function (Restangular) {
+        var getDataList = function (url) {
+            if(url){
+               return Restangular.all('url').getList();
+            }
+            throw "restManaget.getDataList: url parameter is undefined!"
+        };
+
+        var deleteData = function () {
+        };
+
+        var updateData = function () {
+        };
+        return {
+            getData: getData(),
+            deleteData: deleteData(),
+            updateData: updateData()
+        };
     }]);
