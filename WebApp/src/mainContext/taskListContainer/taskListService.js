3c167b5d3b5aca99d02e00c06c1e13398dff5ac2
anton <anton.anikeev.home@gmail.com>
2016-03-28 14:08:32 +0300

    Create basic structure for taskListService

---
+++ WebApp/src/mainContext/taskListContainer/taskListService.js
@@ -0,0 +1,13 @@
+/**
+ * Created by admin on 3/28/2016.
+ */
+//Create taskList
+//Update taskList
+//Delete taskList
+
+//TakeTheTaskListModel
+
+angular.module('mainContext')
+    .service('taskListService',['',function(){
+
+    }]);
